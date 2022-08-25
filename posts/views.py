cf8211602676597b90c6ca5b59d2294b6085d80a
Mario Camarillo <mario.camarillo@itmexicali.edu.mx>
2022-08-25 12:22:58 -0700

    FSDI 113 A1 with a little of A2

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.urls import reverse_lazy
+from .models import Post
+
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "author"]
+
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("post_list")
